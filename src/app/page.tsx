0cf68d6d179b5c2a0871ca423a81aec3763b02e1
MohammedIrfan244 <kuttappuirfan244@gmail.com>
2025-03-28 12:56:38 +0530

    Refactor Navbar component, add MagneticButton, and update global styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import Navbar from '@/components/layout/navbar'
 import React from 'react'
 
 function page() {
   return (
-    <div>
-      Hello
-    </div>
+    <>
+      <Navbar/>
+    </>
   )
 }
 
--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,17 +1,37 @@
+import { Kanit } from 'next/font/google'
 import React from 'react'
-import Image from 'next/image'
-import me from '@/assets/me_circ.png'
+import MagneticButton from '../ui/magneticButton'
 
+const kanit = Kanit({
+    subsets: ["latin"],
+    weight: ['400']
+})
+
+interface ILink {
+    id: string
+    key: string
+}
+const links: ILink[] = [{ id: "about", key: "About Me" }, { id: "project", key: "Projects" }, { id: "skill", key: "Skills" }, { id: "exp", key: "Experience" }, { id: "contact", key: "Contact" }]
 
 function Navbar() {
-  return (
-    <div className='fixed top-0 left-0 w-screen h-20 flex'>
-      <Image src={me} height={30} width={30} alt='Irfan'/>
-      <div className='navigations flex items-center justify-between gap-3'>
-        
-      </div>
-    </div>
-  )
+    return (
+        <nav className='fixed top-0 left-0 w-screen h-16 items-center justify-between flex px-5'>
+            {/* name */}
+            <h2 className={`${kanit.className} text-3xl`}>irfan</h2>
+            {/* navlinks for larger screen */}
+            <div className='items-center justify-between gap-3 hidden sm:flex'>
+                {links.map((link, index) => <li key={index + link.id} className='text-sm list-none hover:opacity-60 hover:cursor-pointer'>{index === links.length - 1 ? (
+                    link.key
+                ) : (
+                    <>
+                        {link.key} <span>&nbsp;&nbsp;&nbsp;&nbsp;/</span>
+                    </>
+                )}</li>)}
+            </div>
+            {/* resume downloader */}
+            <MagneticButton />
+        </nav>
+    )
 }
 
 export default Navbar
