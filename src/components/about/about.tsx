3c9e54828daba13c42afc59911ce4bde96380137
MohammedIrfan244 <kuttappuirfan244@gmail.com>
2025-04-02 17:00:26 +0530

    Add About component with personal description and styling; include about_me image

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 function About() {
   return (
-    <div>
-        about
+    <div id="about" className='px-5 py-10 my-10 sm:my-20'>
+        <div className='flex items-center justify-between h-60'>
+          <h1 className='text-9xl'>About Me</h1>
+          <div className='w-1/2 flex flex-col justify-between h-full'>
+          <h3 className='text-md font-semibold'>Love</h3>
+          <p className=''>I&apos;m <span className='underline'>Mohammed Irfan</span>, a <span className='underline'>MERN stack developer</span> specializing in <span className='underline'>React, Node.js, Express, and Next.js</span>. I focus on building scalable and efficient web applications with a structured and optimized approach.</p>
+          </div>
+        </div>
+        <div className='flex items-center justify-center h-60'>
+          <h3 className=' font-semibold'>What do i do</h3>
+          <p className=''>
+          </p>
+        </div>
     </div>
   )
 }
