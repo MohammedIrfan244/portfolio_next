42a2abaedcfef1d8331498c5b53d680d5831f761
MohammedIrfan244 <kuttappuirfan244@gmail.com>
2025-04-29 16:10:35 +0530

    Remove unused components and assets; clean up globals.css and layout files

--- src/components/ui/dualButton.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function DualButton() {
-  return (
-    <button className="btn-flip" data-back="Download" data-front="Resume"></button>
-  )
-}
-
-export default DualButton
