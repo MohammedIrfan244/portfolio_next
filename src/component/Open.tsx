54dcf1d7945885d5b70ca55ca1cf096642ff4572
MohammedIrfan244 <kuttappuirfan244@gmail.com>
2025-05-20 15:48:42 +0530

    clean up

---
+++ src/component/Open.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Open() {
+  return (
+    <div className='h-screen flex flex-col justify-center items-center'>
+      <p className='text-gray-300 text-sm'>MERNSTACK DEVELOPER</p>
+    </div>
+  )
+}
+
+export default Open
