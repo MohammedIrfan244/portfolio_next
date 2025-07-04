04a925767ea0269e9d9fdbb67725a3c0b0fc3c17
MohammedIrfan244 <kuttappuirfan244@gmail.com>
2025-07-04 14:46:18 +0530

    feat: Update Home component to render Hero, Projects, Profile, and Contact components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+import Contact from '@/component/Contact';
+import Hero from '@/component/hero';
+import Profile from '@/component/profile';
+import Projects from '@/component/Projects';
 import React from 'react';
 
 export default function Home() {
   return (
-    <div>
-      Hello
-    </div>
+    <>
+      <Hero/>
+      <Projects/>
+      <Profile/>
+      <Contact/>
+    </>
   );
 }
