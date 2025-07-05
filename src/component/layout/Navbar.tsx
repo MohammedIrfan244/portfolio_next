1db91cef34bba5014f0ee8eba42e30b8975f2d52
MohammedIrfan244 <kuttappuirfan244@gmail.com>
2025-07-05 11:24:53 +0530

    feat: Update Navbar to use Framer Motion for animations and switch font to Inter

--- src/component/layout/Navbar.tsx
+++ src/component/layout/Navbar.tsx
@@ -1,15 +1,36 @@
 import React from 'react'
-import { montserrat } from '@/lib/font'
+import { inter } from '@/lib/font'
+import {motion} from 'framer-motion'
 
 function Navbar() {
   return (
     <div
-     className={`fixed w-full flex justify-between items-center py-5 md:py-7 lg:py-7 px-6 md:px-7 lg:px-10 font-bold text-stone-700 ${montserrat.className}`}>
-      <a className='text-lg tracking-tighter hover:cursor-pointer mix-blend-difference hover:opacity-75'>Mohammed Irfan</a>
+     className={`fixed w-full flex justify-between items-center py-5 md:py-7 lg:py-7 px-6 md:px-7 lg:px-10 font-bold text-stone-700 ${inter.className}`}>
+      <motion.a
+      initial={{ opacity: 0, y: -20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      href='#'
+       className='text-lg tracking-tighter hover:cursor-pointer mix-blend-difference hover:opacity-75'>Mohammed Irfan</motion.a>
       <div className='flex gap-3 md:gap-5 lg:gap-6 text-base'>
-        <a className='text-sm hover:cursor-pointer hover:opacity-75'>Work</a>
-        <a className='text-sm hover:cursor-pointer hover:opacity-75'>About</a>
-        <a className='text-sm hover:cursor-pointer hover:opacity-75'>Contact</a>
+        <motion.a
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.1 }}
+        href='#'
+         className='text-sm hover:cursor-pointer hover:opacity-75'>Work</motion.a>
+        <motion.a
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.2 }}
+        href='#'
+         className='text-sm hover:cursor-pointer hover:opacity-75'>About</motion.a>
+        <motion.a
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.3 }}
+        href='#'
+         className='text-sm hover:cursor-pointer hover:opacity-75'>Contact</motion.a>
       </div>
     </div>
   )
--- src/lib/font.ts
+++ src/lib/font.ts
@@ -1,5 +1,5 @@
 import { Young_Serif } from "next/font/google";
-import { Montserrat } from "next/font/google";
+import { Inter } from "next/font/google";
 
 
 export const youngSerif = Young_Serif({
@@ -8,7 +8,7 @@ export const youngSerif = Young_Serif({
     weight: ["400"],
     });
 
-export const montserrat = Montserrat({
+export const inter = Inter({
     variable:"--font-montserrat",
     subsets: ["latin"],
     weight: ["400", "500", "600", "700"],
